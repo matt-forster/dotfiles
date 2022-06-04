0a2b869bf0992d16cbfb5c3d7f168d1bd1f824f7
Matt Forster <hey@mattforster.ca>
2022-06-04 16:17:57 -0600

    Update dot_aliases.local Update install.sh

--- install.sh
+++ install.sh
@@ -18,19 +18,16 @@ nix-env -iA nixpkgs.packer
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init matt-forster --apply
 
 wget https://github.com/gopasspw/gopass/releases/download/v1.14.2/gopass_1.14.2_linux_amd64.deb ~
-sudo dpkg -i gopass_1.14.2_linux_amd64.deb
-rm gopass_1.14.2_linux_amd64.deb
+wget -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage ~
+git clone https://github.com/mattmc3/antidote.git ~/.antidote
+git clone https://github.com/so-fancy/diff-so-fancy.git ~/.local/share/diff-so-fancy
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+sudo dpkg -i gopass_1.14.2_linux_amd64.deb
 chmod u+x nvim.appimage
 mv ./nvim.appimage .local/bin/nvim
-
-[ -f ~/.antidote ] && git clone https://github.com/mattmc3/antidote.git ~/.antidote
-
-[ -f ~/local/share/diff-so-fancy ] && git clone https://github.com/so-fancy/diff-so-fancy.git ~/.local/share/diff-so-fancy
 ln -s ~/.local/share/diff-so-fancy/diff-so-fancy ~/.local/bin/diff-so-fancy
 
-[ -f ~/.volta ] && curl https://get.volta.sh | bash 
+curl https://get.volta.sh | bash 
 volta install node
 
 # command -v zsh | sudo tee -a /etc/shells
