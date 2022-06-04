7d3415c4258b3b637546b7f35c6bc4016d5fbae3
Matt Forster <hey@mattforster.ca>
2022-06-04 16:50:03 -0600

    Update dot_zshrc.local.tmpl

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
+#! /usr/bin/env zsh
 cd
 
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init matt-forster --apply
 sh <(curl -L https://nixos.org/nix/install)
 source ~/.nix-profile/etc/profile.d/nix.sh
 
@@ -8,31 +9,23 @@ nix-env -iA nixpkgs.jq
 nix-env -iA nixpkgs.socat
 nix-env -iA nixpkgs.git
 nix-env -iA nixpkgs.zsh
-nix-env -iA nixpkgs.stow
 nix-env -iA nixpkgs.fzf
 nix-env -iA nixpkgs.bat
 nix-env -iA nixpkgs.git-extras
 nix-env -iA nixpkgs.terraform
 nix-env -iA nixpkgs.vault
 nix-env -iA nixpkgs.packer
+nix-env -iA nixpkgs.neovim
+nix-env -iA nixpkgs.gopass
+nix-env -iA nixpkgs.diff-so-fancy
 
-sh -c "$(curl -fsLS chezmoi.io/get)" -- init matt-forster --apply
-
-wget https://github.com/gopasspw/gopass/releases/download/v1.14.2/gopass_1.14.2_linux_amd64.deb ~
-wget -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage ~
 git clone https://github.com/mattmc3/antidote.git ~/.antidote
-git clone https://github.com/so-fancy/diff-so-fancy.git ~/.local/share/diff-so-fancy
-
 source ~/.antidote/antidote.zsh
-sudo dpkg -i gopass_1.14.2_linux_amd64.deb
-chmod u+x nvim.appimage
-mv ./nvim.appimage .local/bin/nvim
-ln -s ~/.local/share/diff-so-fancy/diff-so-fancy ~/.local/bin/diff-so-fancy
 
 antidote bundle <~/.zsh_plugins.txt >~/.zsh_plugins.zsh
 
 curl https://get.volta.sh | bash 
-volta install node
 
+nix-collect-garbage
 # command -v zsh | sudo tee -a /etc/shells
 # chsh -s $(which zsh) $USER
