bb665961b36b2b865a69e9d0d0dc7e6d1e334890
Matt Forster <hey@mattforster.ca>
2022-06-04 10:54:45 -0600

    Update executable_install.sh

--- executable_install.sh
+++ executable_install.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 cd
-sh -c "$(curl -fsLS chezmoi.io/get)" -- init -v matt-forster --apply --one-shot
+
+# Dependencies and packages
+
+apt-get install coreutils build-essential curl git
+
+wget https://github.com/gopasspw/gopass/releases/download/v1.14.2/gopass_1.14.2_linux_amd64.deb
+sudo dpkg -i gopass_1.14.2_linux_amd64.deb
+
+curl https://get.volta.sh | bash 
+
+curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
+
+# Install 
+
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init -v matt-forster --apply
+
+volta install node
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
+asdf update
+
