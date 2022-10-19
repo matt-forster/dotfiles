6bd58b01170d39d2674048c17d88b5a290fc138b
Matthew Forster <hey@mattforster.ca>
2022-10-19 16:37:26 -0600

    feat(git): cleanup config

--- private_dot_config/nixpkgs/git.nix
+++ private_dot_config/nixpkgs/git.nix
@@ -4,8 +4,45 @@
   programs.git = {
     enable = true;
 
+    userName = "Matt Forster";
+    userEmail = "hey@mattforster.ca";
+
+    aliases = {
+      l  = "log --pretty=colored --reverse -10";
+      cb = "create-branch";
+      db = "delete-branch";
+      aa = "add --all";
+      ap = "add --patch";
+      ci = "commit -v";
+      co = "checkout";
+      pf = "push --force-with-lease";
+      aliases = "!git config --get-regexp ^alias\\. | colrm 1 6 | sed 's/[ ]/ = /' | sort";
+    };
+
+    signing = {
+      signByDefault = true;
+      key = "5DA07A957618798A!"; # can we eval this eventually?
+    };
+
+    attributes = [ "* text=auto" "*.pem text eol=lf" ];
+
+    ignores = [ ];
+
+    extraConfig = {
+      branch.autosetuprebase = "always";
+      pull.rebase = "true";
+      push.default = "current";
+      merge.ff = "only";
+      fetch.prune = "true";
+      rebase.autoSquash = "true";
+      color.ui = "auto";
+      core.editor = "nvim";
+      core.autocrlf = "input";
+      pretty.colored = "format:%Cred%h%Creset %s %Cgreen(%cr) %C(bold blue)%an%Creset";
+      init.defaultBranch = "main";
+      diff.algorithm = "patience";
+    };
+
     diff-so-fancy.enable = true;
   };
 }
-
-
