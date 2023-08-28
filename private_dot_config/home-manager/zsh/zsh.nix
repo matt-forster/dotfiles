9431c06a6df279c909f5f0cdbcfb520f436af5b9
Matt Forster <hey@mattforster.ca>
2023-08-28 17:40:33 -0600

    Update private_dot_config/home-manager/zsh/zsh.nix

--- private_dot_config/home-manager/zsh/zsh.nix
+++ private_dot_config/home-manager/zsh/zsh.nix
@@ -7,6 +7,11 @@
     localVariables = {
     };
 
+    envExtra = ''
+      export CC=/usr/bin/clang
+      export CXX=/usr/bin/clang++
+    ''
+
     initExtra = ''
     	source $HOME/.config/home-manager/zsh/init.zsh
     	source $HOME/.config/home-manager/zsh/work.zsh
