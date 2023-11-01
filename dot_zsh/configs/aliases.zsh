91d98c009531d9e7c710c6478338792b791fe7e6
Matt Forster <hey@mattforster.ca>
2023-11-01 17:27:26 -0600

    Update dot_zsh/configs/aliases.zsh

--- dot_zsh/configs/aliases.zsh
+++ dot_zsh/configs/aliases.zsh
@@ -44,5 +44,6 @@ alias cdr='cd $(git rev-parse --show-toplevel)'
 alias clear-history='rm -f ~/.bash_history && rm -f $HISTFILE'
 alias reset-card='gpg-agent-relay stop && gpg-agent-relay start'
 
-alias issues='jira issue list -a$(jira me)'
+alias issues='jira issue list -a$(jira me) --plain'
+alias cissues='jira issue list -a$(jira me) -s"in progress" --plain'
 
