7eef1dfaf7f106e8de425205b42f8952355932c0
Matt Forster <hey@mattforster.ca>
2024-11-05 11:51:14 -0500

    fix(antidote): plugin file locations
    Jira-ticket: none

--- dot_zsh/configs/plugins.zsh
+++ dot_zsh/configs/plugins.zsh
@@ -2,10 +2,10 @@ plugins=(git npm aws)
 autoload -U +X bashcompinit && bashcompinit
 bindkey "^[[3~" delete-char
 
-zsh_plugins=${ZDOTDIR:-$HOME}/.zsh/zsh_plugins
+zsh_plugins=$HOME/.zsh/zsh_plugins
 if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
   (
-    source .antidote/antidote.zsh
+    source $HOME/.antidote/antidote.zsh
     antidote bundle <${zsh_plugins}.txt >${zsh_plugins}.zsh
   )
 fi
