dda994a213f0bfe5e0832d6380a9e018ba1834b9
Matt Forster <hey@mattforster.ca>
2024-12-27 09:25:24 -0700

    Update dot_zsh/configs/plugins.zsh

--- dot_zsh/configs/plugins.zsh
+++ dot_zsh/configs/plugins.zsh
@@ -2,4 +2,15 @@ plugins=(git npm aws)
 autoload -U +X bashcompinit && bashcompinit
 bindkey "^[[3~" delete-char
 
-antidote load
+plugins=(git npm aws)
+autoload -U +X bashcompinit && bashcompinit
+bindkey "^[[3~" delete-char
+
+zsh_plugins=$HOME/.zsh/zsh_plugins
+if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
+  (
+    source $HOME/.antidote/antidote.zsh
+    antidote bundle <${zsh_plugins}.txt >${zsh_plugins}.zsh
+  )
+fi
+source ${zsh_plugins}.zsh
