146307fe3b4a71a772da1ebfd74a9e7e32addebe
Matt Forster <hey@mattforster.ca>
2025-01-07 14:50:25 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+# https://github.com/DeterminateSystems/nix-installer
+
 # May have to rerun if this is missing (as it doesn't get sourced the same everywhere)
 if ! command -v nix &>/dev/null; then
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
