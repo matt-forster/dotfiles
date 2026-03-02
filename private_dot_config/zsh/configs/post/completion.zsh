ebbd73b5af71e2dfcb2cf7bbf8dd075109ebe836
Matt Forster <matt.forster@itmethods.com>
2026-03-02 10:02:56 -0700

    Update .config/zsh/configs/post/completion.zsh

--- private_dot_config/zsh/configs/post/completion.zsh
+++ private_dot_config/zsh/configs/post/completion.zsh
@@ -1,1 +1,5 @@
 # completion fpath and compinit are initialized in plugins.zsh before antidote load
+
+
+mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
+asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
